bb4c677c454504ddc52568a09c1f2882671ecd2f
Bastien Abadie <bastien@nextcairn.com>
2019-06-12 09:05:15 +0200

    bot: add rustfmt unit tests

--- bot/tests/test_lint.py
+++ bot/tests/test_lint.py
@@ -2,6 +2,13 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import json
+import os
+from datetime import datetime
+from unittest.mock import Mock
+from unittest.mock import patch
+
+from conftest import MOCK_DIR
 
 
 def test_flake8_rules(mock_config, mock_revision):
@@ -29,7 +36,7 @@ def test_flake8_rules(mock_config, mock_revision):
 
 def test_as_text(mock_config, mock_revision):
     '''
-    Test text export for ClangTidyIssue
+    Test text export for MozLintIssue
     '''
     from code_review_bot.tasks.lint import MozLintIssue
 
@@ -46,3 +53,68 @@ def test_as_text(mock_config, mock_revision):
         'path': 'test.py',
         'severity': 'error',
     }
+
+
+def test_diff(mock_config, mock_revision):
+    '''
+    Test diff parsing in MozLintIssue
+    '''
+    from code_review_bot.tasks.lint import MozLintIssue
+
+    mock_revision.lines = {
+        'test.rs': [1, 2, 44],
+    }
+
+    issue = MozLintIssue('test.rs', 1, 'error', 42, 'rustfmt', 'nodiff', 'dummy rule', mock_revision)
+    assert str(issue) == 'rustfmt issue error test.rs line 42'
+    assert issue.diff is None
+    assert issue.nb_lines == 1
+    assert not mock_revision.contains(issue)
+
+    diff = '''This
+is
+a
+test'''
+    issue = MozLintIssue('test.rs', 1, 'error', 42, 'rustfmt', 'withdiff', 'dummy rule', mock_revision, diff=diff)
+    assert str(issue) == 'rustfmt issue error test.rs line 42-46'
+    assert issue.diff is not None
+    assert mock_revision.contains(issue)
+
+
+@patch('code_review_bot.tasks.lint.datetime')
+def test_diff_build(mock_datetime, mock_revision):
+    '''
+    Build a full diff from a list of issues
+    '''
+    from code_review_bot.tasks.lint import MozLintTask
+
+    # Set a constant now datetime
+    mock_datetime.utcnow = Mock(return_value=datetime(2019, 1, 1))
+
+    task_status = {
+        'task': {},
+        'status': {
+            'status': 'completed',
+        },
+    }
+    task = MozLintTask('someTaskId', task_status)
+
+    mock_revision.lines = {
+        'path/to/xx.rs': [1, 2, 3, ],
+        'test.rs': [200, 201, 202, 300],
+    }
+    mock_revision.files = mock_revision.lines.keys()
+
+    # Parse issues from mock mozlint artifact
+    path = os.path.join(MOCK_DIR, 'mozlint_rust_issues.json')
+    artifacts = {
+        'public/code-review/mozlint.json': json.load(open(path)),
+    }
+    issues = task.parse_issues(artifacts, mock_revision)
+    assert len(issues) == 6
+
+    # Build patches from issues
+    patches = task.build_patches(artifacts, issues)
+    assert len(patches) == 1
+    path = os.path.join(MOCK_DIR, 'mozlint_rust_issues.diff')
+    assert patches[0] == ('mozlint', open(path).read())
