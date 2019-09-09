3235714cbcd528952154e345e3786d75781a75e3
Bastien Abadie <bastien@nextcairn.com>
2019-09-09 17:11:32 +0200

    Simplify read_requirements

--- events/setup.py
+++ events/setup.py
@@ -3,31 +3,19 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import os.path
+
 import setuptools
+
+here = os.path.dirname(__file__)
 
 
 def read_requirements(file_):
-    lines = []
-    with open(file_) as f:
-        for line in f.readlines():
-            line = line.strip()
-            if (
-                line.startswith("-e ")
-                or line.startswith("http://")
-                or line.startswith("https://")
-            ):
-                extras = ""
-                if "[" in line:
-                    extras = "[" + line.split("[")[1].split("]")[0] + "]"
-                line = line.split("#")[1].split("egg=")[1] + extras
-            elif line == "" or line.startswith("#") or line.startswith("-"):
-                continue
-            line = line.split("#")[0].strip()
-            lines.append(line)
-    return sorted(list(set(lines)))
+    with open(os.path.join(here, file_)) as f:
+        return sorted(list(set(line.split("#")[0].strip() for line in f)))
 
 
-with open("VERSION") as f:
+with open(os.path.join(here, "VERSION")) as f:
     VERSION = f.read().strip()
 
 
