bb6667a76d2f5b3baca70b7312e90646fe4ff52c
Bastien Abadie <bastien@nextcairn.com>
2019-10-18 11:54:09 +0200

    backend: Add models to store Revision, Diff & Issues

---
+++ backend/code_review_backend/issues/admin.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from django.contrib import admin
+
+from code_review_backend.issues.models import Diff
+from code_review_backend.issues.models import Issue
+from code_review_backend.issues.models import Repository
+from code_review_backend.issues.models import Revision
+
+
+class RepositoryAdmin(admin.ModelAdmin):
+    list_display = ("slug", "url")
+
+
+class DiffInline(admin.TabularInline):
+    # Read only inline
+    model = Diff
+    readonly_fields = ("id", "mercurial", "phid", "review_task_id")
+
+
+class RevisionAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "bugzilla_id", "repository")
+    list_filter = ("repository",)
+    inlines = (DiffInline,)
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_filter = ("analyzer",)
+    list_display = ("id", "path", "line", "level", "analyzer", "check", "diff")
+
+
+admin.site.register(Repository, RepositoryAdmin)
+admin.site.register(Revision, RevisionAdmin)
+admin.site.register(Issue, IssueAdmin)
+
+# Naming
+admin.site.site_header = "Mozilla Code Review Backend"
---
+++ backend/code_review_backend/issues/models.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import uuid
+
+from django.db import models
+
+LEVEL_WARNING = "warning"
+LEVEL_ERROR = "error"
+ISSUE_LEVELS = ((LEVEL_WARNING, "Warning"), (LEVEL_ERROR, "Error"))
+
+
+class PhabricatorModel(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    phid = models.CharField(max_length=40, unique=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ("id",)
+
+
+class Repository(PhabricatorModel):
+
+    slug = models.SlugField(unique=True)
+    url = models.URLField(unique=True)
+    try_url = models.URLField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "repositories"
+
+    def __str__(self):
+        return self.slug
+
+
+class Revision(PhabricatorModel):
+    repository = models.ForeignKey(
+        Repository, related_name="revisions", on_delete=models.CASCADE
+    )
+
+    title = models.CharField(max_length=250)
+    bugzilla_id = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return f"D{self.id} - {self.title}"
+
+
+class Diff(PhabricatorModel):
+    revision = models.ForeignKey(
+        Revision, related_name="diffs", on_delete=models.CASCADE
+    )
+
+    review_task_id = models.CharField(max_length=30, unique=True)
+    mercurial = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f"Diff {self.id}"
+
+
+class Issue(models.Model):
+    """An issue detected on a Phabricator patch"""
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    diff = models.ForeignKey(Diff, related_name="issues", on_delete=models.CASCADE)
+
+    # Raw issue data
+    path = models.CharField(max_length=250)
+    line = models.PositiveIntegerField(null=True)
+    nb_lines = models.PositiveIntegerField(null=True)
+    char = models.PositiveIntegerField(null=True)
+    level = models.CharField(max_length=20, choices=ISSUE_LEVELS)
+    check = models.CharField(max_length=250, null=True)
+    message = models.TextField(null=True)
+    analyzer = models.CharField(max_length=50)
+
+    # Calculated hash identifying issue
+    hash = models.CharField(max_length=32)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("diff", "path", "line", "analyzer")
