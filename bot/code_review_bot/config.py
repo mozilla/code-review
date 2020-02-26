95812bc3b3a69636fca59b35d53678bf57ceb5f4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-26 08:30:09 +0100

    bot: Fix treeherder link in task failures reporting, fixes #500 (#499)

--- bot/code_review_bot/config.py
+++ bot/code_review_bot/config.py
@@ -27,7 +27,8 @@ TaskCluster = collections.namedtuple(
     "TaskCluster", "results_dir, task_id, run_id, local"
 )
 RepositoryConf = collections.namedtuple(
-    "RepositoryConf", "name, url, decision_env_revision, decision_env_repository"
+    "RepositoryConf",
+    "name, try_name, url, decision_env_revision, decision_env_repository",
 )
 
 
