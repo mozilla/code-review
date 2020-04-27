8b435e7cf62032701801b00ffbe2a2e5f98030d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-27 15:42:40 +0200

    Use selectedTaskRun instead of selectedJob for the Treeherder link pointing to a given task (#593)
    This way we can avoid a API call to Treeherder.
    
    Fixes #371

--- tools/code_review_tools/treeherder.py
+++ tools/code_review_tools/treeherder.py
@@ -2,9 +2,6 @@
 
 from urllib.parse import urlencode
 
-import slugid
-from thclient import TreeherderClient
-
 JOBS_URL = "https://treeherder.mozilla.org/#/jobs"
 
 
@@ -18,12 +15,6 @@ def get_job_url(repository, revision, task_id=None, run_id=None, **params):
     params.update({"repo": repository, "revision": revision})
 
     if task_id is not None and run_id is not None:
-        treeherder_client = TreeherderClient()
-        uuid = slugid.decode(task_id)
-
-        # Fetch specific job id from treeherder
-        job_details = treeherder_client.get_job_details(job_guid=f"{uuid}/{run_id}")
-        if len(job_details) > 0:
-            params["selectedJob"] = job_details[0]["job_id"]
+        params["selectedTaskRun"] = f"{task_id}-{run_id}"
 
     return f"{JOBS_URL}?{urlencode(params)}"
