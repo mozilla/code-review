d03d75aec5c5ac4f59c5b5d173868b2037b1eccd
EvaBardou <ebardou@teklia.com>
2022-08-02 17:34:18 +0200

    Use sentry_sdk instead of raven and improve Sentry grouping

--- tools/code_review_tools/log.py
+++ tools/code_review_tools/log.py
@@ -3,29 +3,28 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import logging
+import logging.handlers
 import os
+import sys
 
-import logbook
-import logbook.more
-import raven
-import raven.handlers.logbook
+import pkg_resources
+import sentry_sdk
 import structlog
+from sentry_sdk.integrations.logging import LoggingIntegration
+
+root = logging.getLogger()
 
 
-class UnstructuredRenderer(structlog.processors.KeyValueRenderer):
-    def __call__(self, logger, method_name, event_dict):
-        event = None
-        if "event" in event_dict:
-            event = event_dict.pop("event")
-        if event_dict or event is None:
-            # if there are other keys, use the parent class to render them
-            # and append to the event
-            rendered = super(UnstructuredRenderer, self).__call__(
-                logger, method_name, event_dict
-            )
-            return f"{event} ({rendered})"
-        else:
-            return event
+class AppNameFilter(logging.Filter):
+    def __init__(self, project_name, channel, *args, **kwargs):
+        self.project_name = project_name
+        self.channel = channel
+        super().__init__(*args, **kwargs)
+
+    def filter(self, record):
+        record.app_name = f"code-review/{self.channel}/{self.project_name}"
+        return True
 
 
 def setup_papertrail(project_name, channel, PAPERTRAIL_HOST, PAPERTRAIL_PORT):
@@ -34,14 +33,17 @@ def setup_papertrail(project_name, channel, PAPERTRAIL_HOST, PAPERTRAIL_PORT):
     """
 
     # Setup papertrail
-    papertrail = logbook.SyslogHandler(
-        application_name=f"code-review/{channel}/{project_name}",
+    papertrail = logging.handlers.SysLogHandler(
         address=(PAPERTRAIL_HOST, int(PAPERTRAIL_PORT)),
-        level=logbook.INFO,
-        format_string="{record.time} {record.channel}: {record.message}",
-        bubble=True,
     )
-    papertrail.push_application()
+    formatter = logging.Formatter(
+        "%(app_name)s: %(asctime)s %(filename)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    papertrail.setLevel(logging.INFO)
+    papertrail.setFormatter(formatter)
+    papertrail.addFilter(AppNameFilter(project_name, channel))
+    root.addHandler(papertrail)
 
 
 def setup_sentry(name, channel, dsn):
@@ -58,29 +60,29 @@ def setup_sentry(name, channel, dsn):
     else:
         site = "unknown"
 
-    sentry_client = raven.Client(
-        dsn=dsn,
-        site=site,
-        name=name,
-        environment=channel,
-        release=raven.fetch_package_version(f"code-review-{name}"),
+    sentry_logging = LoggingIntegration(
+        level=logging.INFO,  # Capture info and above as breadcrumbs
+        event_level=logging.WARNING,  # Send warnings as events
     )
+    sentry_sdk.init(
+        dsn=dsn,
+        integrations=[sentry_logging],
+        server_name=name,
+        environment=channel,
+        release=pkg_resources.get_distribution(f"code-review-{name}").version,
+    )
+    sentry_sdk.set_tag("site", site)
 
     if task_id is not None:
         # Add a Taskcluster task id when available
         # It will be shown in the Additional Data section on the dashboard
-        sentry_client.context.merge({"extra": {"task_id": task_id}})
-
-    sentry_handler = raven.handlers.logbook.SentryHandler(
-        sentry_client, level=logbook.WARNING, bubble=True
-    )
-    sentry_handler.push_application()
+        sentry_sdk.set_context("task", {"task_id": task_id})
 
 
 def init_logger(
     project_name,
     channel=None,
-    level=logbook.INFO,
+    level=logging.INFO,
     PAPERTRAIL_HOST=None,
     PAPERTRAIL_PORT=None,
     SENTRY_DSN=None,
@@ -89,36 +91,33 @@ def init_logger(
     if not channel:
         channel = os.environ.get("APP_CHANNEL")
 
-    # Output logs on stderr, with color support on consoles
-    fmt = "{record.time} [{record.level_name:<8}] {record.channel}: {record.message}"
-    handler = logbook.more.ColorizedStderrHandler(level=level, format_string=fmt)
-    handler.push_application()
+    logging.basicConfig(
+        format="%(asctime)s.%(msecs)06d [%(levelname)-8s] %(filename)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        stream=sys.stdout,
+        level=level,
+    )
 
     # Log to papertrail
     if channel and PAPERTRAIL_HOST and PAPERTRAIL_PORT:
         setup_papertrail(project_name, channel, PAPERTRAIL_HOST, PAPERTRAIL_PORT)
 
-    # Log to senty
+    # Log to sentry
     if channel and SENTRY_DSN:
         setup_sentry(project_name, channel, SENTRY_DSN)
 
-    def logbook_factory(*args, **kwargs):
-        # Logger given to structlog
-        logbook.compat.redirect_logging()
-        return logbook.Logger(level=level, *args, **kwargs)
-
-    # Setup structlog over logbook, with args list at the end
+    # Setup structlog
     processors = [
         structlog.stdlib.PositionalArgumentsFormatter(),
         structlog.processors.StackInfoRenderer(),
         structlog.processors.format_exc_info,
-        UnstructuredRenderer(),
+        structlog.stdlib.render_to_log_kwargs,
     ]
 
     structlog.configure(
-        context_class=structlog.threadlocal.wrap_dict(dict),
         processors=processors,
-        logger_factory=logbook_factory,
+        context_class=structlog.threadlocal.wrap_dict(dict),
+        logger_factory=structlog.stdlib.LoggerFactory(),
         wrapper_class=structlog.stdlib.BoundLogger,
         cache_logger_on_first_use=True,
     )
