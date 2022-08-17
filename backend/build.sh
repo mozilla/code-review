da898e1a5cecae20209f5a70322c409090f2ad50
EvaBardou <ebardou@teklia.com>
2022-08-17 11:16:42 +0200

    Fix CI

--- backend/build.sh
+++ backend/build.sh
@@ -1,16 +1,14 @@
-#!/bin/bash
-
+#!/bin/sh
 COMMIT_SHA=$1
 VERSION=$2
 SOURCE=$3
-BUILD_URL=$4
-CHANNEL=$5
+CHANNEL=$4
 
-# create a version.json per https://github.com/mozilla-services/Dockerflow/blob/master/docs/version_object.md
+# Create a version.json per https://github.com/mozilla-services/Dockerflow/blob/master/docs/version_object.md
 printf '{"commit": "%s", "version": "%s", "source": "%s", "build": "%s"}\n' \
 "$COMMIT_SHA" \
 "$VERSION" \
 "$SOURCE" \
-"$BUILD_URL" > backend/code_review_backend/version.json
+"${TASKCLUSTER_ROOT_URL}/tasks/${TASK_ID}" > backend/code_review_backend/version.json
 
-taskboot build --image mozilla/code-review --tag "$CHANNEL" --tag "$COMMIT_SHA" --write /backend.tar backend/Dockerfile
+taskboot --target /code-review build --image mozilla/code-review --tag "$CHANNEL" --tag "$COMMIT_SHA" --write /backend.tar backend/Dockerfile
