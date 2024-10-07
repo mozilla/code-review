8213802d032425088726eb3a12d73d630259c9d3
mlbonhomme <bonhomme@teklia.com>
2024-10-07 11:48:21 +0200

    Update MockPhabricator after libmozdata 0.2.8 (required user agent)

--- bot/tests/conftest.py
+++ bot/tests/conftest.py
@@ -773,18 +773,6 @@ class MockPhabricator:
         self.build_messages = collections.defaultdict(list)
         self.transactions = collections.defaultdict(list)
 
-        config_file = tempfile.NamedTemporaryFile()
-        with open(config_file.name, "w") as f:
-            custom_conf = ConfigParser()
-            custom_conf.add_section("User-Agent")
-            custom_conf.set("User-Agent", "name", "libmozdata")
-            custom_conf.write(f)
-            f.seek(0)
-
-        from libmozdata import config
-
-        config.set_config(config.ConfigIni(config_file.name))
-
         endpoints = {
             "differential.createcomment": self.comment,
             "differential.createinline": self.comment_inline,
