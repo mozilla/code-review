3916a22fe0df4d64dafae48f14929ae9e1f5e520
Bastien Abadie <bastien@nextcairn.com>
2024-11-25 16:31:38 +0100

    Run backend unit tests with postgresql

---
+++ backend/ci/setup_postgres.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -ex
+export PG_VERSION=15
+
+# Silent apt
+export DEBIAN_FRONTEND=noninteractive
+
+# Install postgresql
+apt update
+apt install -qq -y postgresql-$PG_VERSION
+
+# Setup access rights
+cp $(dirname $0)/pg_hba.conf /etc/postgresql/$PG_VERSION/main/pg_hba.conf
+
+# Start postgresql
+pg_ctlcluster $PG_VERSION main start
+
+# Create user & database
+su postgres -c 'createuser --createdb tester'
+su postgres -c 'createdb --owner=tester code-review'
