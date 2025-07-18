20efb04bc5e8ce6a198f3b806d36179d0c7c3adc
Valentin Rigal <rigal@teklia.com>
2025-07-18 15:19:39 +0200

    Add similarity=95 to hg import

--- bot/code_review_bot/mercurial.py
+++ bot/code_review_bot/mercurial.py
@@ -366,6 +366,7 @@ class Repository:
                     patches=io.BytesIO(patch.patch.encode("utf-8")),
                     message=message.encode("utf-8"),
                     user=user.encode("utf-8"),
+                    similarity=95,
                 )
             except Exception as e:
                 logger.info(
