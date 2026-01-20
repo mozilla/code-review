e71bd6d780421eff9dbe8a635484410a5ba5d8ab
Bastien Abadie <bastien@nextcairn.com>
2026-01-20 16:28:37 +0100

    Expose as __all__

--- bot/code_review_bot/revisions/__init__.py
+++ bot/code_review_bot/revisions/__init__.py
@@ -2,5 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from code_review_bot.revisions.base import ImprovementPatch, Revision  # noqa
-from code_review_bot.revisions.phabricator import PhabricatorRevision  # noqa
+from code_review_bot.revisions.base import ImprovementPatch, Revision
+from code_review_bot.revisions.phabricator import PhabricatorRevision
+
+__all__ = [ImprovementPatch, Revision, PhabricatorRevision]
