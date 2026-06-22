dbe0ed82d7ce1c1cdb3922b70989a028029a3c12
Valentin Rigal <rigal@teklia.com>
2026-06-22 15:53:59 +0200

    Wait for database to be ready

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -4,6 +4,14 @@
 # Make non-zero exit codes & other errors fatal.
 set -euo pipefail
 
+# Ensure the database accepts connections
+echo "Checking database status at $DATABASE_URL"
+while ! nc -z db 5432; do
+    echo "-----> Waiting for PostgreSQL server to be ready"
+    sleep 1;
+done
+echo "-----> PostgreSQL service is available"
+
 # Run the migrations
 ./manage.py migrate --noinput
 
